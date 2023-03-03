06468ff3a7a7def705e667520a3f36f82105ed0f
mwihoti <86232216+mwihoti@users.noreply.github.com>
2023-03-03 08:48:35 +0300

    modern app

--- bank_modern_app/src/components/Navbar.jsx
+++ bank_modern_app/src/components/Navbar.jsx
@@ -2,7 +2,8 @@ import {useState} from 'react'
 import { close, logo, menu} from '../assets';
 import { navLinks } from '../constants';
 
-function Navbar() {
+const Navbar = () => {
+    const [toggle, setToggle] = useState(false);
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="hoobank" className="w-[124px] h-[32px]"/>
@@ -10,7 +11,7 @@ function Navbar() {
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
             <li key={nav.id}
-            className={`font-poppins font-normal cursor-pointer text-[16px] text-white`}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white `}
             >
                 <a href={`#${nav.id}`}>
                     {nav.title}
@@ -19,6 +20,27 @@ function Navbar() {
         ))}
 
       </ul>
+      <div clasName="sm:hidden flex flex-1 justify-end items-center">
+        <img src={toggle ? close : menu}
+        alt ="menu"
+        className= "w-[28px] h-[28px] object-contain"
+        onClick={() => setToggle((prev) => !prev)}
+        />
+      </div>
+      <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-x1 sidebar`}>
+      <ul className="list-none flex flex-col justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+            <li key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'} text-white `}
+            >
+                <a href={`#${nav.id}`}>
+                    {nav.title}
+                </a>
+            </li>
+        ))}
+
+      </ul>
+      </div>
     </nav>
   )
 }
